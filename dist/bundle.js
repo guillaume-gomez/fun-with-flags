e1ded11d8bd04cf8cfd6da199e422e8b92a7c56c
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-04-14 21:25:18 +0200

    :tada: setup

---
+++ dist/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************!*\
+  !*** ./src/app.ts ***!
+  \********************/
+console.log("hello world !");
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map

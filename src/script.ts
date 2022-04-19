4cea75abcd0df7f8433eeb4b9873c701d39bdb00
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-04-20 00:01:11 +0200

    add select and images to load countries flags

--- src/script.ts
+++ src/script.ts
@@ -4,15 +4,8 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import gsap from 'gsap';
 import cv, { Mat, MatVector } from "opencv-ts";
 import { generateGeometries } from "./detectionToGeometry";
-
 // Scene
 const scene = new THREE.Scene();
-// Object
-/*const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00Afff });
-const mesh = new THREE.Mesh(geometry, material);*/
-//scene.add(mesh);
-
 // Sizes
 const sizes = {
     width: 500, //window.innerWidth,
@@ -36,13 +29,12 @@ renderer.setSize(sizes.width, sizes.height);
 
 // Controls
 const controls = new OrbitControls( camera, renderer.domElement );
+controls.enablePan = true;
 
 /**
  * Animate
  */
 //gsap.to(mesh.rotation, { duration: 1, x: 5, repeat:-1 });
-
-
 function tick()
 {
     // Render
@@ -99,60 +91,58 @@ window.addEventListener('dblclick', () =>
 
 
 cv.onRuntimeInitialized = () => {
-  const imgElement = document.getElementById('imageSrc') as HTMLImageElement;
-  const inputElement = document.getElementById('fileInput');
+  const selectElement = document.getElementById('country-flags') as HTMLImageElement;
 
-  if(inputElement && imgElement) {
-
-    inputElement.addEventListener('change', (e : any) => {
-      if(e && e.target && e.target.files) {
-        (imgElement as HTMLImageElement).src = URL.createObjectURL(e.target.files[0]);
+  if(selectElement) {
+    selectElement.addEventListener('change', (e : any) => {
+      if(e && e.target) {
+        loadImage(e.target.value);
       }
     }, false);
 
-    imgElement.onload = () => {
-      const src = cv.imread(imgElement);
-      const { width, height } = imgElement;
-      const greyScaleImage: Mat = new cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
-      const binaryThreshold: Mat = new cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
-      const inverseBinaryThreshold: Mat = new cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
-      const dst: Mat = new cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
-      
-      cv.cvtColor(src, greyScaleImage, cv.COLOR_RGBA2GRAY, 0);
-      cv.threshold(greyScaleImage, binaryThreshold, 100, 200, cv.THRESH_BINARY);
-      cv.threshold(greyScaleImage, inverseBinaryThreshold, 100, 200, cv.THRESH_BINARY_INV);
-      
-      let contours : MatVector = new cv.MatVector();
-      let hierarchy : Mat = new cv.Mat();
-      cv.findContours(binaryThreshold, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
-      let meshes = generateGeometries(contours, hierarchy, src);
-      scene.add(...meshes);
-      console.log(hierarchy.channels())
-
-      contours.delete();
-      hierarchy.delete();
-      
-      contours = new cv.MatVector();
-      hierarchy = new cv.Mat();
-      cv.findContours(inverseBinaryThreshold, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
-      meshes = generateGeometries(contours, hierarchy, src);
-      scene.add(...meshes);
-      
-
-      // draw contours with random Scalar
-      for (let i = 0; i < contours.size(); ++i) {
-        const color = new cv.Scalar(
-            Math.round(Math.random() * 255),
-            Math.round(Math.random() * 255),
-            Math.round(Math.random() * 255)
-        );
-        cv.drawContours(dst, contours, i, color, 5, cv.LINE_8, hierarchy, 100);
-      }
-      cv.imshow('canvasOutput', binaryThreshold);
-      src.delete();
-      dst.delete();
-      contours.delete();
-      hierarchy.delete();
-    };
   }
 };
+
+
+function loadImage(imageDomId :string) {
+  const src = cv.imread(imageDomId);
+  const greyScaleImage: Mat = new cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
+  const binaryThreshold: Mat = new cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
+  const inverseBinaryThreshold: Mat = new cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
+  const dst: Mat = new cv.Mat.zeros(src.rows, src.cols, cv.CV_8UC3);
+
+  cv.cvtColor(src, greyScaleImage, cv.COLOR_RGBA2GRAY, 0);
+  cv.threshold(greyScaleImage, binaryThreshold, 100, 200, cv.THRESH_BINARY);
+  cv.threshold(greyScaleImage, inverseBinaryThreshold, 100, 200, cv.THRESH_BINARY_INV);
+
+  let contours : MatVector = new cv.MatVector();
+  let hierarchy : Mat = new cv.Mat();
+  cv.findContours(binaryThreshold, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
+  let meshes = generateGeometries(contours, hierarchy, src);
+  scene.add(...meshes);
+
+  contours.delete();
+  hierarchy.delete();
+
+  contours = new cv.MatVector();
+  hierarchy = new cv.Mat();
+  cv.findContours(inverseBinaryThreshold, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
+  meshes = generateGeometries(contours, hierarchy, src);
+  scene.add(...meshes);
+
+
+  // draw contours with random Scalar
+  for (let i = 0; i < contours.size(); ++i) {
+  const color = new cv.Scalar(
+      Math.round(Math.random() * 255),
+      Math.round(Math.random() * 255),
+      Math.round(Math.random() * 255)
+  );
+  cv.drawContours(dst, contours, i, color, 5, cv.LINE_8, hierarchy, 100);
+  }
+  //cv.imshow('canvasOutput', binaryThreshold);
+  src.delete();
+  dst.delete();
+  contours.delete();
+  hierarchy.delete();
+}

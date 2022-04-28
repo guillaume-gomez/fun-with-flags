6658e974119215ed3e0babf84f22bfba41dec261
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-04-29 00:02:23 +0200

    :lipstick: finish to install daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  plugins: [require('daisyui')],
-};
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}

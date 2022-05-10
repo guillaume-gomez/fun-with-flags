9f83f87c990a703b57f2156d0453811c4a493e78
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-05-10 18:19:50 +0200

    add custom rules for some flags

--- src/flagsConfig.ts
+++ src/flagsConfig.ts
@@ -11,10 +11,16 @@ export interface FlagData {
 }
 
 const additionalFlagParams : FlagData[] = [
+    { key: "al", name: "Albania", threshold: { min: 1, max: 50 }, override: true },
+    { key: "aq", name: "Antarctica", threshold: { min: 184, max: 236 }, override: true},
+    { key: "aw", name: "Aruba", threshold: { min: 182, max: 246 }, override: true },
+    { key: "am", name: "Armenia", threshold: { min: 55, max: 255 }, override: true },
+    { key: "at", name: "Austria", threshold: { min: 216, max: 255 }, override: true },
+    { key: "az", name: "Azerbaijan", threshold: { min: 106, max: 137 }, override: true },
+    { key: "co", name: "Colombie", threshold: { min: 65, max: 100 }, override: true},
     { key: "fr", name: "France", threshold: { min: 140, max: 242 }, override: true },
     { key: "ci", name: "Côte d'Ivoire (Ivory Coast)", threshold: { min: 250, max: 255 }, override: true },
 ];
-
 
 export function generateFlagParams() : FlagData[] {
     const flags : Object = require("./flagsName.json");
